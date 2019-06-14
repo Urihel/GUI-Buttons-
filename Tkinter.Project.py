0868b6691ebfc235846616884e79715c649cd88d
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-06-14 01:47:01 -0700

    Update Tkinter.Project.py

--- Tkinter.Project.py
+++ Tkinter.Project.py
@@ -2,18 +2,14 @@ import tkinter
 top = tkinter.Tk()
 #This creates a basic button layout
 
-b1 = tkinter.Button(top, text ="Click Here", command = top, height = 5, width = 20)
-b2 = tkinter.Button(top, text="Press Me Down", command = top, height = 5, width = 20)
-b3 = tkinter.Button(top, text="Push My Buttons", command = top, height = 5, width = 20)
-b4 = tkinter.Button(top, text="Push it", command = top, height = 5, width = 20)
+b1 = tkinter.Button(top, text ="Click Here", command = top, height = 5, width = 20, bd = 3, activebackground = 'red')
+b2 = tkinter.Button(top, text="Press Me Down", command = top, height = 5, width = 20, bd = 3, activebackground = 'blue')
+b3 = tkinter.Button(top, text="Push My Buttons", command = top, height = 5, width = 20, bd = 3, activebackground = 'green')
+b4 = tkinter.Button(top, text="Push it", command = top, height = 5, width = 20, bd = 3, activebackground = 'limegreen')
 b1.pack()
 b2.pack()
 b3.pack()
 b4.pack()
-
-
-
-
 
 
 
