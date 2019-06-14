32b2215930622fb3b697a8f499e32b0d4260d8f5
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-06-14 14:47:27 -0700

    Update Tkinter.Project.py

--- Tkinter.Project.py
+++ Tkinter.Project.py
@@ -1,16 +1,23 @@
 import tkinter
-top = tkinter.Tk()
-#This creates a basic button layout
 
-b1 = tkinter.Button(top, text ="Click Here", command = top, height = 5, width = 20, bd = 3, activebackground = 'red')
-b2 = tkinter.Button(top, text="Press Me Down", command = top, height = 5, width = 20, bd = 3, activebackground = 'blue')
-b3 = tkinter.Button(top, text="Push My Buttons", command = top, height = 5, width = 20, bd = 3, activebackground = 'green')
-b4 = tkinter.Button(top, text="Push it", command = top, height = 5, width = 20, bd = 3, activebackground = 'limegreen')
+top = tkinter.Tk()
+
+#This creates a basic button layout
+b1 = tkinter.Button(top, text ="Click Here", command = top, height = 3, width = 14,
+                    bd = 3, activebackground = 'white', font = ('Times',14))
+b2 = tkinter.Button(top, text="Press Me Down", command = top, height = 3, width = 14,
+                    bd = 3, activebackground = 'green', font = ('Times',14))
+b3 = tkinter.Button(top, text="Push My Buttons", command = top, height = 3, width = 14,
+                    bd = 3, activebackground = 'gray', font = ('Times',14))
+b4 = tkinter.Button(top, text="Push it", command = top, height = 3, width = 14,
+                    bd = 3, activebackground = 'grey', font = ('Times',14))
 b1.pack()
 b2.pack()
 b3.pack()
 b4.pack()
 
-
-
-
+#will change the background of button completely
+b1.configure(bg="#00D1FF")
+b2.configure(bg="#78FF00")
+b3.configure(bg="#FF0042")
+b4.configure(bg="#FF3200")
