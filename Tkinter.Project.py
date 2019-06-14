02f993f429472114151966c7c2f838e5749ab3aa
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-06-14 01:20:20 -0700

    Add files via upload

---
+++ Tkinter.Project.py
@@ -0,0 +1,18 @@
+import tkinter
+top = tkinter.Tk()
+#This creates a basic button layout
+
+
+b1 = tkinter.Button(top, text ="Audi Parts", command = top, height = 5, width = 20)
+b2 = tkinter.Button(top, text="BMW Parts", command = top, height = 5, width = 20)
+b3 = tkinter.Button(top, text="Mercedes Part", command = top, height = 5, width = 20)
+b4 = tkinter.Button(top, text="Porsche", command = top, height = 5, width = 20)
+b5 = tkinter.Button(top, text="Porsche", command = top, height = 5, width = 20)
+b1.pack()
+b2.pack()
+b3.pack()
+b4.pack()
+b5.pack()
+
+
+
